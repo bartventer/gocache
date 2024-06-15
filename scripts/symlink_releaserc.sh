54a0b2c63b4e51662fc90502e876a023a5dab9c5
Bart Venter <bartventer@outlook.com>
2024-06-15 08:50:01 +0000

    chore: Update release workflow tag formats

--- scripts/symlink_releaserc.sh
+++ scripts/symlink_releaserc.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-# This script creates a symlink to .releaserc.json in the same directory as go.mod files.
+# This script creates a copy of .releaserc.json in the same directory as go.mod files and adds a "tagFormat" field to it.
 
 set -euo pipefail
 
-echo "(*) Creating symlinks to .releaserc.json in the same directory as go.mod files"
+echo "(*) Creating copies of .releaserc.json in the same directory as go.mod files and adding a 'tagFormat' field"
 
 # Find all go.mod files
 gomods=$(find . -name go.mod)
 
-# create the symlink
+# create the copy and add the "tagFormat" field
 for file in $gomods; do
   dir=$(dirname "$file")
   echo "==> Checking $dir"
   if [[ ! -e $dir/.releaserc.json ]]; then
-    # Calculate the relative path to .releaserc.json
-    target=$(realpath --relative-to="$dir" .releaserc.json)
-    ln -s "$target" "$dir"/.releaserc.json
-    echo "  OK. Created symlink $dir/.releaserc.json"
+    # Use the absolute path to .releaserc.json
+    target=$(realpath .releaserc.json)
+    cp "$target" "$dir"/.releaserc.json
+    jq --arg dir "${dir#./}" '. + {tagFormat: "\($dir)/v${version}"}' "$dir"/.releaserc.json >"$dir"/temp.json && mv "$dir"/temp.json "$dir"/.releaserc.json
+    echo "  OK. Created copy $dir/.releaserc.json and added 'tagFormat' field"
   else
     echo "  Already exists. Skipping $dir/.releaserc.json"
   fi
