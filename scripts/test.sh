3f4d8668ec18e3f3ed29aafaf58713a426b41313
Bart Venter <bartventer@outlook.com>
2024-06-15 17:09:36 +0000

    chore(scripts): Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 COVERPROFILE="${COVERPROFILE:-coverage.out}"
+WORKSPACE="${GITHUB_WORKSPACE:-$(git rev-parse --show-toplevel)}"
 
 # Clear the coverage file
 echo "mode: atomic" > "$COVERPROFILE"
@@ -14,11 +15,12 @@ for file in $gomods; do
     dir=$(dirname "$file")
     pushd "$dir" >/dev/null
     # Create a temporary coverage file for this module
-    go test -v -race -coverprofile="tmp.out" -covermode=atomic -timeout 15m ./...
-    if [ -f "tmp.out" ]; then
+    tmpfile=$(mktemp --suffix=".out" --tmpdir="$WORKSPACE")
+    go test -v -race -outputdir="$WORKSPACE" -coverprofile="$tmpfile" -covermode=atomic -timeout 15m ./...
+    if [[ -f "$tmpfile" ]]; then
         # Skip the mode line and append to the main coverage file
-        tail -n +2 "tmp.out" >> "$COVERPROFILE"
-        rm "tmp.out"
+        tail -n +2 "$tmpfile" >> "$COVERPROFILE"
+        rm "$tmpfile"
     fi
     popd >/dev/null
 done
